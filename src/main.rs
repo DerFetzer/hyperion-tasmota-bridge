4664f6c89da75c2d242a8f3ebb501df3f09961da
DerFetzer <kontakt@der-fetzer.de>
2021-02-24 18:04:28 +0100

    Add first WLED implementation, fix reverse mapping

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = vec![0; settings.receive_buffer_size.unwrap_or(1024) as usize];
     let mut old_buf = vec![0; settings.receive_buffer_size.unwrap_or(1024) as usize];
 
-    let socket = UdpSocket::bind(settings.udp_bind_address).await?;
+    let receive_socket = UdpSocket::bind(settings.udp_bind_address).await?;
+    let send_socket = UdpSocket::bind("0.0.0.0:12345").await?;
 
     let create_opts = CreateOptionsBuilder::new()
         .server_uri(settings.mqtt.url)
@@ -42,10 +43,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     mqtt_client.connect(conn_opts).await?;
 
     loop {
-        let (len, _) = socket.recv_from(&mut buf).await?;
+        let (len, _) = receive_socket.recv_from(&mut buf).await?;
 
-        if len > buf.len() {
-            warn!("Receive buffer size is too low! ({} < {})", buf.len(), len);
+        if len == buf.len() {
+            warn!("Receive buffer size might be too low!");
         }
         trace!("Received buffer: {:?}", &buf[..]);
 
@@ -67,8 +68,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         Right(0..mapping.length.unwrap_or(1))
                     };
 
-                    for i in range.into_iter() {
-                        colors[(mapping.target_start - min_target_index + i) as usize] = Some(
+                    for (i, j) in range.into_iter().enumerate() {
+                        colors[(mapping.target_start - min_target_index + j) as usize] = Some(
                             buf[mapping.source_start as usize * 3 + i as usize * 3 + 2] as u32
                                 + ((buf[mapping.source_start as usize * 3 + i as usize * 3 + 1]
                                 as u32)
@@ -94,6 +95,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     ))
                     .await?;
             }
+        }
+        for wled in settings.wleds.iter() {
+            let max_target_index = wled.mappings.iter().map(|m| m.target_start + m.length.unwrap_or(1)).max().unwrap();
+
+            let mut packet = vec![0_u8; (max_target_index * 3 + 2) as usize];
+            packet[0] = 2; // DRGB
+            packet[1] = 1; // Timeout
+
+            for mapping in wled.mappings.iter() {
+                let range = if mapping.reverse.unwrap_or(false) {
+                    Left((0..mapping.length.unwrap_or(1)).rev())
+                }
+                else {
+                    Right(0..mapping.length.unwrap_or(1))
+                };
+
+                for (i, j) in range.into_iter().enumerate() {
+                    let buffer_start_index = mapping.source_start as usize * 3 + i * 3;
+                    let target_start_index = (mapping.target_start as usize + j as usize) * 3 + 2;
+                    packet[target_start_index..target_start_index + 3].copy_from_slice(&buf[buffer_start_index..buffer_start_index + 3]);
+                }
+            }
+
+            debug!("UDP payload: {:?}", packet);
+            send_socket.send_to(packet.as_slice(), &wled.url).await?;
         }
     }
 }
--- src/settings.rs
+++ src/settings.rs
@@ -15,6 +15,13 @@ pub struct Tasmota {
 }
 
 #[derive(Debug, Deserialize)]
+pub struct Wled {
+    pub url: String,
+    pub mappings: Vec<LedMapping>,
+    pub number_of_leds: u16,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct LedMapping {
     pub source_start: u16,
     pub target_start: u16,
@@ -28,6 +35,7 @@ pub struct Settings {
     pub mqtt: Mqtt,
     pub receive_buffer_size: Option<u32>,
     pub tasmotas: Vec<Tasmota>,
+    pub wleds: Vec<Wled>,
 }
 
 impl Settings {
@@ -40,28 +48,37 @@ impl Settings {
         let mut res: Settings = s.try_into()?;
 
         for tasmota in res.tasmotas.iter_mut() {
-            if tasmota.mappings.is_empty() {
-                return Err(ConfigError::Message("There has to be at least one mapping per device!".to_string()));
-            }
-
-            tasmota.mappings.sort_by(|a, b| a.target_start.cmp(&b.target_start));
-            // Check for overlapping target ranges
-            let f = tasmota.mappings.iter().fold(Some(0), |i, t| {
-                i.and_then(|i| {
-                    if i > t.target_start {
-                        None
-                    }
-                    else {
-                        Some(t.target_start + t.length.unwrap_or(1))
-                    }
-                })
-            });
-
-            if f.is_none() {
-                return Err(ConfigError::Message("Overlapping target ranges are not allowed!".to_string()));
-            }
+            Settings::process_mappings(&mut tasmota.mappings)?;
+        }
+        for wled in res.wleds.iter_mut() {
+            Settings::process_mappings(&mut wled.mappings)?;
         }
 
         Ok(res)
+    }
+
+    fn process_mappings(mappings: &mut Vec<LedMapping>) -> Result<(), ConfigError> {
+        if mappings.is_empty() {
+            return Err(ConfigError::Message("There has to be at least one mapping per device!".to_string()));
+        }
+
+        mappings.sort_by(|a, b| a.target_start.cmp(&b.target_start));
+        // Check for overlapping target ranges
+        let f = mappings.iter().fold(Some(0), |i, t| {
+            i.and_then(|i| {
+                if i > t.target_start {
+                    None
+                }
+                else {
+                    Some(t.target_start + t.length.unwrap_or(1))
+                }
+            })
+        });
+
+        if f.is_none() {
+            return Err(ConfigError::Message("Overlapping target ranges are not allowed!".to_string()));
+        }
+
+        return Ok(())
     }
 }
