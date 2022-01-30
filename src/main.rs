31afd7b42aa32eff56bc0d0fd7ddab029a49b41a
DerFetzer <kontakt@der-fetzer.de>
2022-01-30 12:22:29 +0100

    Update dependencies, remove MQTT, use async task for processing

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ use crate::settings::Settings;
 use either::*;
 use env_logger::Env;
 use log::{debug, info, trace, warn};
-use paho_mqtt::{AsyncClient, ConnectOptionsBuilder, CreateOptionsBuilder, Message};
-use std::time::Duration;
 use tokio::net::UdpSocket;
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -17,30 +16,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let settings = Settings::new()?;
 
-    let mut buf = vec![0; settings.receive_buffer_size.unwrap_or(1024) as usize];
-    let mut old_buf = vec![0; settings.receive_buffer_size.unwrap_or(1024) as usize];
+    info!("{:?}", settings);
 
-    let receive_socket = UdpSocket::bind(settings.udp_bind_address).await?;
+    let mut buf = vec![0; settings.clone().receive_buffer_size.unwrap_or(1024) as usize];
+
+    let receive_socket = UdpSocket::bind(settings.clone().udp_bind_address).await?;
     let send_socket = UdpSocket::bind("0.0.0.0:12345").await?;
 
-    let create_opts = CreateOptionsBuilder::new()
-        .server_uri(settings.mqtt.url)
-        .client_id(settings.mqtt.client_id)
-        .finalize();
+    let (tx_chan, mut rx_chan) = mpsc::channel::<Vec<u8>>(1_000);
 
-    let mut conn_opts_builder = ConnectOptionsBuilder::new();
+    tokio::spawn(async move {
+        while let Some(buf) = rx_chan.recv().await {
+            for wled in settings.wleds.iter() {
+                let mut packet = vec![0_u8; (wled.number_of_leds * 3 + 2) as usize];
+                packet[0] = 2; // DRGB
+                packet[1] = 1; // Timeout
 
-    conn_opts_builder.automatic_reconnect(Duration::new(5, 0), Duration::new(10, 0));
+                wled.mappings.iter().for_each(|mapping| {
+                    let range = if mapping.reverse.unwrap_or(false) {
+                        Left((0..mapping.length.unwrap_or(1)).rev())
+                    } else {
+                        Right(0..mapping.length.unwrap_or(1))
+                    };
 
-    if settings.mqtt.user.is_some() {
-        conn_opts_builder.user_name(settings.mqtt.user.unwrap());
-        conn_opts_builder.password(settings.mqtt.password.unwrap_or(String::new()));
-    }
+                    for (i, j) in range.into_iter().enumerate() {
+                        let buffer_start_index = mapping.source_start as usize * 3 + i * 3;
+                        let target_start_index =
+                            (mapping.target_start as usize + j as usize) * 3 + 2;
+                        packet[target_start_index..target_start_index + 3]
+                            .copy_from_slice(&buf[buffer_start_index..buffer_start_index + 3]);
+                    }
+                });
 
-    let conn_opts = conn_opts_builder.finalize();
-
-    let mqtt_client = AsyncClient::new(create_opts).unwrap();
-    mqtt_client.connect(conn_opts).await?;
+                debug!("UDP payload: {:?}", packet);
+                send_socket
+                    .send_to(packet.as_slice(), &wled.url)
+                    .await
+                    .unwrap();
+            }
+        }
+    });
 
     loop {
         let (len, _) = receive_socket.recv_from(&mut buf).await?;
@@ -49,75 +64,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             warn!("Receive buffer size might be too low!");
         }
         trace!("Received buffer: {:?}", &buf[..]);
-
-        if buf.iter().zip(old_buf.iter()).any(|(a, b)| a != b) {
-            info!("Change in buffer...sending MQTT");
-            old_buf.copy_from_slice(&buf);
-
-            for tasmota in settings.tasmotas.iter().flatten() {
-                let min_target_index = tasmota.mappings.iter().map(|m| m.target_start).min().unwrap();
-                let max_target_index = tasmota.mappings.iter().map(|m| m.target_start + m.length.unwrap_or(1)).max().unwrap();
-
-                let mut colors = vec![Option::<u32>::None; (max_target_index - min_target_index) as usize];
-
-                for mapping in tasmota.mappings.iter() {
-                    let range = if mapping.reverse.unwrap_or(false) {
-                        Left((0..mapping.length.unwrap_or(1)).rev())
-                    }
-                    else {
-                        Right(0..mapping.length.unwrap_or(1))
-                    };
-
-                    for (i, j) in range.into_iter().enumerate() {
-                        colors[(mapping.target_start - min_target_index + j) as usize] = Some(
-                            buf[mapping.source_start as usize * 3 + i as usize * 3 + 2] as u32
-                                + ((buf[mapping.source_start as usize * 3 + i as usize * 3 + 1]
-                                as u32)
-                                << 8)
-                                + ((buf[mapping.source_start as usize * 3 + i as usize * 3]
-                                as u32)
-                                << 16)
-                        )
-                    }
-                }
-                let mut payload =
-                    String::with_capacity((max_target_index - min_target_index) as usize * 8);
-
-                colors.iter().for_each(|c| payload.push_str(&format!("#{:06x} ", c.unwrap_or(0))));
-
-                debug!("MQTT payload for {}: {}", tasmota.mqtt_prefix, payload);
-
-                mqtt_client
-                    .publish(Message::new(
-                        format!("{}/LED{}", tasmota.mqtt_prefix, min_target_index + 1),
-                        payload,
-                        1,
-                    ))
-                    .await?;
-            }
-        }
-        for wled in settings.wleds.iter().flatten() {
-            let mut packet = vec![0_u8; (wled.number_of_leds * 3 + 2) as usize];
-            packet[0] = 2; // DRGB
-            packet[1] = 1; // Timeout
-
-            for mapping in wled.mappings.iter() {
-                let range = if mapping.reverse.unwrap_or(false) {
-                    Left((0..mapping.length.unwrap_or(1)).rev())
-                }
-                else {
-                    Right(0..mapping.length.unwrap_or(1))
-                };
-
-                for (i, j) in range.into_iter().enumerate() {
-                    let buffer_start_index = mapping.source_start as usize * 3 + i * 3;
-                    let target_start_index = (mapping.target_start as usize + j as usize) * 3 + 2;
-                    packet[target_start_index..target_start_index + 3].copy_from_slice(&buf[buffer_start_index..buffer_start_index + 3]);
-                }
-            }
-
-            debug!("UDP payload: {:?}", packet);
-            send_socket.send_to(packet.as_slice(), &wled.url).await?;
-        }
+        tx_chan.send(buf[..len].to_vec()).await?;
     }
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,27 +1,13 @@
 use config::{Config, ConfigError, Environment, File};
 
-#[derive(Debug, Deserialize)]
-pub struct Mqtt {
-    pub url: String,
-    pub client_id: String,
-    pub user: Option<String>,
-    pub password: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Tasmota {
-    pub mqtt_prefix: String,
-    pub mappings: Vec<LedMapping>,
-}
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Wled {
     pub url: String,
     pub mappings: Vec<LedMapping>,
     pub number_of_leds: u16,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct LedMapping {
     pub source_start: u16,
     pub target_start: u16,
@@ -29,32 +15,23 @@ pub struct LedMapping {
     pub reverse: Option<bool>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
     pub udp_bind_address: String,
-    pub mqtt: Mqtt,
     pub receive_buffer_size: Option<u32>,
-    pub tasmotas: Option<Vec<Tasmota>>,
-    pub wleds: Option<Vec<Wled>>,
+    pub wleds: Vec<Wled>,
 }
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        let mut s = Config::new();
+        let s = Config::builder()
+            .add_source(File::with_name("config.yml").required(false))
+            .add_source(Environment::with_prefix("htb"))
+            .build()?;
 
-        s.merge(File::with_name("config.yml").required(false))?;
-        s.merge(Environment::with_prefix("htb"))?;
+        let mut res: Settings = s.try_deserialize()?;
 
-        let mut res: Settings = s.try_into()?;
-
-        if res.tasmotas.is_none() && res.wleds.is_none() {
-            return Err(ConfigError::Message("There are no target devices configured!".to_string()));
-        }
-
-        for tasmota in res.tasmotas.iter_mut().flatten() {
-            Settings::process_mappings(&mut tasmota.mappings)?;
-        }
-        for wled in res.wleds.iter_mut().flatten() {
+        for wled in res.wleds.iter_mut() {
             Settings::process_mappings(&mut wled.mappings)?;
         }
 
@@ -63,7 +40,9 @@ impl Settings {
 
     fn process_mappings(mappings: &mut Vec<LedMapping>) -> Result<(), ConfigError> {
         if mappings.is_empty() {
-            return Err(ConfigError::Message("There has to be at least one mapping per device!".to_string()));
+            return Err(ConfigError::Message(
+                "There has to be at least one mapping per device!".to_string(),
+            ));
         }
 
         mappings.sort_by(|a, b| a.target_start.cmp(&b.target_start));
@@ -72,17 +51,18 @@ impl Settings {
             i.and_then(|i| {
                 if i > t.target_start {
                     None
-                }
-                else {
+                } else {
                     Some(t.target_start + t.length.unwrap_or(1))
                 }
             })
         });
 
         if f.is_none() {
-            return Err(ConfigError::Message("Overlapping target ranges are not allowed!".to_string()));
+            return Err(ConfigError::Message(
+                "Overlapping target ranges are not allowed!".to_string(),
+            ));
         }
 
-        return Ok(())
+        return Ok(());
     }
 }
